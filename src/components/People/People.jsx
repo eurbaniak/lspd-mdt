4aa51961885beaee5bca134440e5fa892a8ef289
Edwin <50717284+Adderall-dev@users.noreply.github.com>
2022-09-08 05:06:48 +0200

    add input in People tab

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import "./people.css";
 
 const People = () => {
-  return <div className="people">People</div>;
+  const { register, handleSubmit } = useForm();
+  return (
+    <div className="people">
+      <input {...register("search")} />
+    </div>
+  );
 };
 
 export default People;
