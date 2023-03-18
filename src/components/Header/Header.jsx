e724e1a00eaac4a8c5dd13505d2f08df59a0a266
Edwin <50717284+Adderall-dev@users.noreply.github.com>
2023-03-18 19:20:35 +0100

    handle logout, add searching logic in People tab

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,21 @@ import { useAuth } from "../../userContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase";
 import style from "./header.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const user = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    await signOut(auth);
+    navigate("/");
+  };
 
   return (
     <div className={style.header}>
       <h1>Police</h1>
-      {user.user ? <a onClick={() => signOut(auth)}>Logout</a> : null}
+      {user.user ? <a onClick={handleSignOut}>Logout</a> : null}
     </div>
   );
 };
--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -1,12 +1,66 @@
-import React from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import "./people.css";
+import { app } from "../../firebase";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+
+const db = getFirestore(app);
 
 const People = () => {
   const { register, handleSubmit } = useForm();
+  const [people, setPeople] = useState([]);
+
+  const onSubmit = async (data) => {
+    const searchTerm = data.searchTerm.trim();
+    const searchPhrase = searchTerm.toLowerCase();
+
+    const q = query(
+      collection(db, "people"),
+      where("FullName", ">=", searchPhrase),
+      where("FullName", "<=", searchPhrase + "\uf8ff")
+    );
+
+    getDocs(q).then((querySnapshot) => {
+      const results = [];
+      querySnapshot.forEach((doc) => {
+        results.push(doc.data());
+      });
+
+      console.log(results);
+      if (results.length === 0) {
+        setPeople(null); // no results found
+      } else {
+        setPeople(results);
+      }
+    });
+  };
+
   return (
     <div className="people">
-      <input {...register("search")} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("searchTerm")}
+          placeholder="Search by name or surname"
+        />
+        <button type="submit">Search</button>
+      </form>
+      {people ? (
+        people.map((person) => (
+          <div key={person.id}>
+            <p>Name: {person.name}</p>
+            <p>Surname: {person.surname}</p>
+            <p>Age: {person.age}</p>
+            {/* Add any other fields you want to display */}
+          </div>
+        ))
+      ) : (
+        <p>No results found</p>
+      )}
     </div>
   );
 };
